0dc4ac84111f7dfb33657a9359a46f2c2f7afd38
Gus Vargas <gvargas@hubspot.com>
2020-12-15 09:33:31 -0600

    include dist

---
+++ dist/0.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/lazy.js":
+/*!*********************!*\
+  !*** ./src/lazy.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n\n//# sourceURL=webpack:///./src/lazy.js?");
+
+/***/ })
+
+}]);
---
+++ dist/child-0.js
@@ -0,0 +1,14 @@
+{
+
+/***/ "./src/lazy.js":
+/*!*********************!*\
+  !*** ./src/lazy.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n\n//# sourceURL=webpack:///./src/lazy.js?");
+
+/***/ })
+
+};
